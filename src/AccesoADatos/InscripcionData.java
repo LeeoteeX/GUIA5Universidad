c67ade6be56e3740457e34353a8ae0aa2ac484e3
LeeoteeX <pilincho93@gmail.com>
2023-09-12 11:43:35 -0300

    InscripcionData
    agregado los metodos guardarInscripcion, ObtenerInscripciones y ObtenerInscripcionesPorAlumno

--- src/AccesoADatos/InscripcionData.java
+++ src/AccesoADatos/InscripcionData.java
@@ -5,10 +5,88 @@
  */
 package AccesoADatos;
 
+import Entidades.Inscripcion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+
 /**
  *
  * @author Leandro
  */
 public class InscripcionData {
-    
+
+    Connection conn = null;
+    MateriaData matData;
+    AlumnoData aluData;
+
+    public InscripcionData() {
+
+        conn = Conexion.getConexion();
+    }
+
+    public void guardarInscripcion(Inscripcion insc) {
+
+        String sql = "INSERT INTO inscripcion (nota, idAlumno, idMateria) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setDouble(1, insc.getNota());
+            ps.setInt(2, insc.getIdAlumno());
+            ps.setInt(3, insc.getIdMaterial());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de Conexion. GUARDAR INSCRIPCION" + ex.getMessage());
+        }
+    }
+
+    public List<Inscripcion> obtenerInscripciones() {
+
+        String sql = "SELECT * FROM inscripcion";
+        List<Inscripcion> listaInsc = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Inscripcion inscripcion = new Inscripcion();
+                inscripcion.setIdAlumno(rs.getInt("idAlumno"));
+                inscripcion.setIdInscripto(rs.getInt("idInscripto"));
+                inscripcion.setIdMaterial(rs.getInt("idMateria"));
+                listaInsc.add(inscripcion);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de Conexion, OBTENER INSCRIPCIONES" + ex.getMessage());
+        }
+        return listaInsc;
+    }
+
+    public List<Inscripcion> obtenerInscripcionesPorAlumno(int id) {
+
+        String sql = "SELECT * FROM inscripcion WHERE idAlumno = ?";
+        List<Inscripcion> listaInsc = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Inscripcion inscripcion = new Inscripcion();
+                inscripcion.setIdAlumno(rs.getInt("idAlumno"));
+                inscripcion.setIdInscripto(rs.getInt("idInscripto"));
+                inscripcion.setIdMaterial(rs.getInt("idMateria"));
+                listaInsc.add(inscripcion);
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de Conexion, OBTENER INSCRIPCIONES POR ALUMNO" + ex.getMessage());
+        }
+        return listaInsc;
+    }
+
 }
--- src/Entidades/Inscripcion.java
+++ src/Entidades/Inscripcion.java
@@ -6,6 +6,47 @@ public class Inscripcion {
     private int idInscripto;
     private double nota;
     private int idAlumno;
-    private int idMaterial;
+    private int idMateria;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(double nota, int idAlumno, int idMaterial) {
+        this.nota = nota;
+        this.idAlumno = idAlumno;
+        this.idMateria = idMaterial;
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public int getIdMaterial() {
+        return idMateria;
+    }
+
+    public void setIdMaterial(int idMaterial) {
+        this.idMateria = idMaterial;
+    }
 
 }
